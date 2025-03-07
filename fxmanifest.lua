bfb13ba3ef25fd441620dd97417e77bfe1d0f2bf
GamingLuke1337 <64887613+GamingLuke1337@users.noreply.github.com>
2025-03-07 15:30:25 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,16 +1,19 @@
 fx_version 'adamant'
-
 game 'gta5'
 lua54 'yes'
 
-version '1.0'
-shared_script '@ox_lib/init.lua'
+author 'GamingLuke1337 & fabryyzzz'
+description ' A Mechanic Menu working with ox_lib'
+version '1.0.0'
 
-
-client_scripts {
-    'client.lua',
-}
+client_script 'client/main.lua'
 
 server_scripts {
-    "server.lua"
+    'server/main.lua',
+    'server/update.lua'
+}
+
+shared_scripts {
+    '@ox_lib/init.lua',
+    'config.lua'
 }
